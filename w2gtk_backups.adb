6c53fdb916929188730e8fb7496639002294bb47
Juan L. Freniche <fdesp87@gmail.com>
2025-11-01 12:17:27 +0100

    mend

--- w2gtk_backups.adb
+++ w2gtk_backups.adb
@@ -30,45 +30,45 @@ package body W2gtk_Backups is
    --  Perform Patch  --
    ---------------------
 
-   function Perform_Patch (The_Path          : String;
-                           Filename_With_Ext : String) return Integer;
-   function Perform_Patch (The_Path          : String;
-                           Filename_With_Ext : String) return Integer is
-      Status    : aliased Integer;
-      Args      : Argument_List (1 .. 3);
-   begin
-      Args (1) := new String'("--verbose");
-      --  Args (1) := new String'("-s");
-      Args (2) := new String'(The_Path & "/" & Filename_With_Ext);
-      Args (3) := new String'(Args (2).all & ".patch");
-         Debug (-1, "patch " & Args (1).all
-                & " " & Args (2).all
-                & " " & Args (3).all);
-      declare
-         Output : constant String := Get_Command_Output
-           (Command    => "/usr/bin/patch",
-            Arguments  => Args,
-            Input      => "",
-            Status     => Status'Access,
-            Err_To_Out => True);
-      begin
-         if Status /= 0 then
-            Debug (-1, "FAILED to execute command: "
-                   & "/usr/bin/patch "
-                   & Argument_List_To_String (Args)
-                   & " status " & Img (Status));
-         else
-            Debug (-1, Sp (3) & "patched " & Output);
-         end if;
-         Free (Args);
-         return Status;
-      exception
-         when Invalid_Process =>
-            Free (Args);
-            Debug (-1, "FAILED to spawn patch");
-            return -1;
-      end;
-   end Perform_Patch;
+   --  function Perform_Patch (The_Path          : String;
+   --                          Filename_With_Ext : String) return Integer;
+   --  function Perform_Patch (The_Path          : String;
+   --                          Filename_With_Ext : String) return Integer is
+   --     Status    : aliased Integer;
+   --     Args      : Argument_List (1 .. 3);
+   --  begin
+   --     Args (1) := new String'("--verbose");
+   --     --  Args (1) := new String'("-s");
+   --     Args (2) := new String'(The_Path & "/" & Filename_With_Ext);
+   --     Args (3) := new String'(Args (2).all & ".patch");
+   --        Debug (-1, "patch " & Args (1).all
+   --               & " " & Args (2).all
+   --               & " " & Args (3).all);
+   --     declare
+   --        Output : constant String := Get_Command_Output
+   --          (Command    => "/usr/bin/patch",
+   --           Arguments  => Args,
+   --           Input      => "",
+   --           Status     => Status'Access,
+   --           Err_To_Out => True);
+   --     begin
+   --        if Status /= 0 then
+   --           Debug (-1, "FAILED to execute command: "
+   --                  & "/usr/bin/patch "
+   --                  & Argument_List_To_String (Args)
+   --                  & " status " & Img (Status));
+   --        else
+   --           Debug (-1, Sp (3) & "patched " & Output);
+   --        end if;
+   --        Free (Args);
+   --        return Status;
+   --     exception
+   --        when Invalid_Process =>
+   --           Free (Args);
+   --           Debug (-1, "FAILED to spawn patch");
+   --           return -1;
+   --     end;
+   --  end Perform_Patch;
 
    --------------------
    --  Perform Diff  --
@@ -110,7 +110,9 @@ package body W2gtk_Backups is
                TIO.Put (PatchFile, Output);
                TIO.Close (PatchFile);
 
-               Status := Perform_Patch (The_Path, Filename_With_Ext);
+               --  Status := Perform_Patch (The_Path, Filename_With_Ext);
+               Debug (-1, "Patch file generated. Review the patch and use to recover changes");
+               Debug (-1, "");
             else
                Debug (-1, "  files identical, no patching");
             end if;
